07dbb77721620059ba5726d3f9af303b249532bf
Zarif Rasul <zarif@The-Scribe>
2022-08-17 13:55:08 -0600

    includes code for github/PAT

--- test script.R
+++ test script.R
@@ -1,3 +1,12 @@
 a = 2
 b = 3
 a+b
+
+#install.packages("usethis") 
+library(usethis)
+?use_github
+edit_r_environ()
+use_github(protocol = 'https', auth_token = Sys.getenv("GITHUB_PAT"))
+``
+
+#committed the above for reference 
